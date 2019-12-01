fa337bf9cf54a1bb03817ecdf88e436f1082d6f6
Josue Miguel <josmiguel92@gmail.com>
2019-11-30 19:06:00 -0500

    adding document entity

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Repository\DocumentRepository;
 use App\Repository\ProductCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function MongoDB\BSON\fromJSON;
 
 class AdminController extends AbstractController
 {
@@ -80,6 +82,96 @@ class AdminController extends AbstractController
         }
 
         return new JsonResponse($items);
+    }
+
+
+
+    /**
+     * @Route("/api/documents", name="api_document")
+     * @param ProductCategoryRepository $categoryRepository
+     * @return JsonResponse
+     */
+    public function api_document(DocumentRepository $documentRepository): JsonResponse
+    {
+        $docs = $documentRepository->findAll();
+
+
+        $items = [];
+
+        foreach ($docs as $doc)
+        {
+
+
+            $items[] = [
+                'id' => $doc->getId(),
+                'title' => $doc->getTitle(),
+                'file' => $doc->getFileWebPath(),
+                'created_on' =>  $doc->getCreatedAt(),
+                'modified_on' =>  $doc->getUpdatedAt(),
+                'image' => $doc->getImageWebPath()
+            ];
+
+        }
+
+        return new JsonResponse($items);
+    }
+
+
+
+    /**
+     * @Route("/api/contact", name="api_contact")
+     * @return JsonResponse
+     */
+    public function api_contact(): JsonResponse
+    {
+
+        $contactInfo = <<<EOF
+{
+    "opening_times": [
+        {
+            "day_range": "Monday - Friday",
+            "hour_range": "09:00 - 17:00"
+        },
+        {
+            "day_range": "Saturday - Sunday (and public holidays)",
+            "hour_range": "10:00 - 16:00"
+        }
+    ],
+    "emails": [
+        {
+            "label": "General Contact",
+            "address": "info@sensesofcuba.com"
+        },
+        {
+            "label": "Sales",
+            "address": "info@sensesofcuba.com"
+        },
+        {
+            "label": "Product Management",
+            "address": "info@sensesofcuba.com"
+        }
+    ],
+    "phones": [
+        {
+            "label": "General",
+            "number": "+53 7866 4734"
+        },
+        {
+            "label": "Emergency",
+            "number": "+53 7866 4734"
+        }
+    ],
+    "addresses": [
+        {
+            "address": "Edificio Bacardi, Oficina 404, Calle Monserrate 261, CP 10100, La Habana Vieja, Habana, Cuba"
+        }
+    ]
+}
+EOF;
+
+        return JsonResponse::fromJsonString($contactInfo)
+            ->setSharedMaxAge(300);
+
     }
 
 
