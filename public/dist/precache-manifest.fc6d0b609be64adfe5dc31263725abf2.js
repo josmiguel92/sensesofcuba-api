10bd0148f34eb3a2d9a131c9bef2ff76c8ff0e31
lexz451 <lexz.zaldivar@gmail.com>
2019-12-31 20:12:59 -0500

    Frontend project integration

---
+++ public/dist/precache-manifest.fc6d0b609be64adfe5dc31263725abf2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eefbe8ca72019adb468",
+    "url": "/css/app.ba629074.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4d6f363d9a81e2444fdaafd392c32fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05255570c81cd762c23",
+    "url": "/js/about.02203dd3.js"
+  },
+  {
+    "revision": "7eefbe8ca72019adb468",
+    "url": "/js/app.56757675.js"
+  },
+  {
+    "revision": "c704909d83c4ccd5070e",
+    "url": "/js/chunk-vendors.f7978236.js"
+  },
+  {
+    "revision": "944602594c3a3eb8483b05fb26e57cc6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
