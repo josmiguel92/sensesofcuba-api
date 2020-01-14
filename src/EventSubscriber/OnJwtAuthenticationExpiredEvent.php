6d59f3caa56a48613ba926de6e5f29168d367995
Josue Miguel <josmiguel92@gmail.com>
2020-01-14 01:01:45 -0500

    adding redirection from admin on token expired

---
+++ src/EventSubscriber/OnJwtAuthenticationExpiredEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTAuthenticatedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTExpiredEvent;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\EventListener\ResponseListener;
+
+class OnJwtAuthenticationExpiredEvent extends AbstractController implements EventSubscriberInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.response' => [
+                'redirectToHomeResponseFromAdmin',
+            ]
+        ];
+    }
+
+
+
+    public function redirectToHomeResponseFromAdmin(ResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        if($response->getStatusCode() === 401 && $event->getRequest()->get('_route') == 'easyadmin')
+        {
+            $event->setResponse($this->redirectToRoute('homepage'));
+        }
+    }
+
+}
