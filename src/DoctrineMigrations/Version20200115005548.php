3949d6d522c3485e1ecc0e9909d4fd406a3532c8
Josue Miguel <josmiguel92@gmail.com>
2020-01-15 00:08:26 -0500

    fix migration

--- src/DoctrineMigrations/Version20200115005548.php
+++ src/DoctrineMigrations/Version20200115005548.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Application\Migrations;
+namespace Application\DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
